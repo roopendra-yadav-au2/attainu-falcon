82f66d23bcb8752f619d2107ecb3e25bcf98b8f7
Rupen <roopendra.153@gmail.com>
2019-06-10 09:27:53 +0530

    Coding Challenge

---
+++ coding-challenges/challenge 15/script.js
@@ -0,0 +1,20 @@
+function nextLetter(string)
+{
+var i, nextString="";
+for(i=0;i<string.length;i++)
+{
+  var code=string.charCodeAt(i);
+  
+  if(code==90)
+  code=65;
+  else if(code==122)
+  code=97;
+  else 
+  code=code +1;
+ nextString = nextString + String.fromCharCode(code);
+ 
+}
+return nextString;
+}
+var str=nextLetter("hello");
+console.log(str);
